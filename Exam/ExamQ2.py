e7265ebf17f3c23939e89a203d757044cdfcfe0a
Frederikke Larsen <zpf492@alumni.ku.dk>
2023-05-25 16:25:57 +0200

    question 4

--- Exam/ExamQ2.py
+++ Exam/ExamQ2.py
@@ -1,5 +1,6 @@
 import sympy as sp
 import numpy as np
+from scipy.optimize import minimize_scalar
 
 class HairSalon:
     def __init__(self, kappa, eta, w):
@@ -104,3 +105,55 @@ class HairSalon3:
             h_value = self.calculate_h(epsilon_series)
             h_values.append(h_value)
         return np.mean(h_values)
+    
+
+#Question 4
+
+
+
+class HairSalonoptimal:
+    def __init__(self, rho, eta, wage, iota, sigma_epsilon, R):
+        self.rho = rho
+        self.eta = eta
+        self.wage = wage
+        self.iota = iota
+        self.sigma_epsilon = sigma_epsilon
+        self.R = R
+
+    def calculate_h(self, epsilon_series, delta):
+        kappa_series = [1.0]  # Initial kappa
+        ell_series = [0]  # Initial ell
+        h_value = 0.0
+
+        for t in range(120):
+            # Calculate kappa_t based on AR(1) process
+            kappa_t = np.exp(self.rho * np.log(kappa_series[-1]) + epsilon_series[t])
+            kappa_series.append(kappa_t)
+
+            # Calculate ell_t based on the policy with threshold delta
+            ell_star = ((1 - self.eta) * kappa_t / self.wage) ** (1 / self.eta)
+            if t > 0 and abs(ell_series[-1] - ell_star) > delta:
+                ell_t = ell_star
+            else:
+                ell_t = ell_series[-1]
+            ell_series.append(ell_t)
+
+            # Calculate the period profit
+            profit = kappa_t * ell_t ** (1 - self.eta) - self.wage * ell_t
+            adjustment_cost = self.iota if ell_t != ell_series[-2] else 0
+            period_value = profit - adjustment_cost
+            discounted_value = period_value * self.R ** (-t)
+            h_value += discounted_value
+
+        return h_value
+
+    def objective_function(self, delta):
+        K = 1000  # Number of shock series to simulate
+        h_values = []
+
+        for _ in range(K):
+            epsilon_series = np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon, size=120)
+            h_value = self.calculate_h(epsilon_series, delta)
+            h_values.append(h_value)
+
+        return -np.mean(h_values)  # Negate to maximize the objective function
