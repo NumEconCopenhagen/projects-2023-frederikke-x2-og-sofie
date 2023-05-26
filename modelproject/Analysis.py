a84b0f90f85dd455164078e83979f95ed3b1bdcf
bpz801 <bpz801@alumni.ku.dk>
2023-05-26 10:15:49 +0200

    Model project

--- modelproject/Analysis.py
+++ modelproject/Analysis.py
@@ -17,8 +17,8 @@ class AnalysismodelclassOLG():
         #All the parameters and the variables are being defined
 
         #The variables and parameters used for the utility function 
-        par.c_y1t = sm.symbols('c_1t')           #Consumption in period t, when young.      
-        par.c_o2t = sm.symbols('c_{2t+1}')       #Consumption in period t+1, when old. 
+        par.c_y1t = sm.symbols('c_1t')          #Consumption in period t, when young.      
+        par.c_o2t = sm.symbols('c_{2t+1}')      #Consumption in period t+1, when old. 
         par.rho = sm.symbols('rho')             #The time preference and/or patience  
 
         #The variables and parameters used for the budget constraints
@@ -61,14 +61,14 @@ class AnalysismodelclassOLG():
     def budget_con(self):
         par = self.par
         #The intertemporal budget constraint function is set up
-        # self refers to the variables and paremeters from the "setup(self)"
-        # Return is a sympy function, which returns the functino for the intertemoral budget constraint, which is solved when 
-        # Right hand side (RHS) equals the left hand side (LHS)
+        #self refers to the variables and paremeters from the "setup(self)"
+        #Return is a sympy function, which returns the functino for the intertemoral budget constraint, which is solved when 
+        #Right hand side (RHS) equals the left hand side (LHS)
         
         #Defining following equations:
         d_t1 = par.tau * par.w_t1                                               #The benefit when old 
-        bc_t1 = sm.Eq(par.c_y1t + par.s_t, (1-par.tau) * par.w_t)                #Budget constraint when young
-        bc_t2 = sm.Eq(par.c_o2t, par.s_t * (1 + par.r_t1)+ (1 + par.n) * d_t1)   #The budget constraint when old
+        bc_t1 = sm.Eq(par.c_y1t + par.s_t, (1-par.tau) * par.w_t)               #Budget constraint when young
+        bc_t2 = sm.Eq(par.c_o2t, par.s_t * (1 + par.r_t1)+ (1 + par.n) * d_t1)  #The budget constraint when old
         bc_t2_s = sm.solve(bc_t2, par.s_t)                                      #Saving is solved by using the budget constraint when old using sympy
 
         #Inserting savings into the first budget constraint
@@ -84,24 +84,24 @@ class AnalysismodelclassOLG():
         # self refers to the variables and paremeters from the "setup(self)"
         # Return is a sympy function, which returns the Euler equation denoted as euler_1
         lagrange = self.utility_func() + par.lamb * self.budget_con()   #The Lagrangian is definded
-        foc1 = sm.Eq(0, sm.diff(lagrange, par.c_y1t))                  #The firs order condition wrt the consumption when young
-        foc2 = sm.Eq(0, sm.diff(lagrange, par.c_o2t))                  #The firs order condition wrt the consumption when old
-        lamb_1 = sm.solve(foc1, par.lamb)[0]                          #The lambda is solved for in the FOC, when young
-        lamb_2 = sm.solve(foc2, par.lamb)[0]                          #The lambda is solved for in the FOC, when old
-        euler_1 = sm.solve(sm.Eq(lamb_1,lamb_2), par.c_y1t)[0]         #The Euler equation is defined
+        foc1 = sm.Eq(0, sm.diff(lagrange, par.c_y1t))                   #The firs order condition wrt the consumption when young
+        foc2 = sm.Eq(0, sm.diff(lagrange, par.c_o2t))                   #The firs order condition wrt the consumption when old
+        lamb_1 = sm.solve(foc1, par.lamb)[0]                            #The lambda is solved for in the FOC, when young
+        lamb_2 = sm.solve(foc2, par.lamb)[0]                            #The lambda is solved for in the FOC, when old
+        euler_1 = sm.solve(sm.Eq(lamb_1,lamb_2), par.c_y1t)[0]          #The Euler equation is defined
         return sm.Eq(euler_1, par.c_y1t)
     
     
     def opt_save(self):
         par = self.par
-        # The equations used to find the optimal saving is defined
-        # self refers to the variables and paremeters from the "setup(self)"
-        # Return is a sympy function, which returns the optimal saving denoted as saving
+        #The equations used to find the optimal saving is defined
+        #self refers to the variables and paremeters from the "setup(self)"
+        #Return is a sympy function, which returns the optimal saving denoted as saving
         d_t1 = par.tau * par.w_t1                               #Defining the benefit when old 
         bc_t1 = (1-par.tau) * par.w_t - par.s_t                 #Defining the budget constraint when young
         bc_t2 = par.s_t * (1 + par.r_t1)+ (1 + par.n) * d_t1    #Defining the budget constraint when old
 
-        # 4. The budget constraints are substituted into the Euler equation
+        #The budget constraints are substituted into the Euler equation
         eul = self.euler_eq() 
         sav = (eul.subs(par.c_y1t , bc_t1)).subs(par.c_o2t , bc_t2)
                 
@@ -114,9 +114,9 @@ class AnalysismodelclassOLG():
     
     def cap_acc(self):
         par = self.par
-        # The equations used to find the caital accumulation is defined
-        # self refers to the variables and paremeters from the "setup(self)"
-        # Return is a sympy function, which returns the capital accumulaiton denoted as k_t
+        #The equations used to find the caital accumulation is defined
+        #self refers to the variables and paremeters from the "setup(self)"
+        #Return is a sympy function, which returns the capital accumulaiton denoted as k_t
 
         #Auxiliary variables are being defined, to make it easier to refer to later
         a = (1 / (1 + (1+par.rho)/(2+par.rho)*((1-par.alpha)/par.alpha) * par.tau))
@@ -132,9 +132,9 @@ class AnalysismodelclassOLG():
         
     def cap_steadystate(self):
         par = self.par
-        # The equations used to find the steady sate for caital is defined
-        # self refers to the variables and paremeters from the "setup(self)"
-        # Return is a sympy function, which returns the steady state for capital denoted as k_star
+        #The equations used to find the steady sate for caital is defined
+        #self refers to the variables and paremeters from the "setup(self)"
+        #Return is a sympy function, which returns the steady state for capital denoted as k_star
         
         #Auxiliary variables are being defined, to make it easier to refer to later
         a = (1 / (1 + (1+par.rho)/(2+par.rho)*((1-par.alpha)/par.alpha) * par.tau))
