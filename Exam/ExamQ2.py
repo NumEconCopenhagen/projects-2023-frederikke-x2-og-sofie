62511588eaea405de4ffd13bfa4bd15c2f9b4345
Frederikke Larsen <zpf492@alumni.ku.dk>
2023-05-26 13:55:52 +0200

    Update

--- Exam/ExamQ2.py
+++ Exam/ExamQ2.py
@@ -24,205 +24,203 @@ class HairSalonQ1:
 #question 2 
 
 class HairSalonQ2:
-    def __init__(self, rho, eta, wage, iota, sigma_epsilon, R):
-        #Setting the parameters 
+    def __init__(self, rho, iota, sigma_epsilon, R, eta, w):
         self.rho = rho
-        self.eta = eta
-        self.wage = wage
         self.iota = iota
         self.sigma_epsilon = sigma_epsilon
         self.R = R
+        self.eta = eta
+        self.w = w
 
-    def calculate_h(self, epsilon_series):
-        #Calculating the ex-post value of the hair salon conditional on the shock series 
-        kappa_series = [1.0]  # Initial kappa
-        ell_series = [0]  # Initial ell
-        h_value = 0.0
+    def policy(self, kappa_t):
+        return ((1 - self.eta) * kappa_t / self.w) ** (1 / self.eta)
+
+    def simulate_shock_series(self):
+        epsilon = np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon, size=120)
+        kappa = [1]
+        ell = [0]
 
         for t in range(120):
-            # Calculate kappa_t based on AR(1) process
-            kappa_t = np.exp(self.rho * np.log(kappa_series[-1]) + epsilon_series[t])
-            kappa_series.append(kappa_t)
+            kappa_t = self.rho * np.log(kappa[t]) + epsilon[t]
+            ell_t = self.policy(kappa_t)
+            kappa.append(np.exp(kappa_t))
+            ell.append(ell_t)
 
-            # Calculate l_t based on the policy from Question 1
-            ell_t = ((1 - self.eta) * kappa_t / self.wage) ** (1 / self.eta)
-            ell_series.append(ell_t)
+        return kappa, ell
 
-            # Calculate the period profit
-            profit = kappa_t * ell_t ** (1 - self.eta) - self.wage * ell_t
-            adjustment_cost = self.iota if ell_t != ell_series[-2] else 0 #adjustment_cost is set to iota if ell_t is not equal to
-            #the last element in the ell_series list; otherwise, it is set to 0.
-            period_value = profit - adjustment_cost
-            discounted_value = period_value * self.R ** (-t)
-            h_value += discounted_value
+    def calculate_salon_value(self, kappa, ell):
+        salon_value = sum([(self.R ** -t) * (kappa[t] * ell[t] ** (1 - self.eta) - self.w * ell[t] -
+                                              (ell[t] != ell[t-1]) * self.iota)
+                           for t in range(120)])
+        return salon_value
 
-        return h_value
+    def calculate_ex_ante_value(self, K):
+        total_value = 0
 
-    def calculate_expected_h(self, K):
-        #Calculate the expected value of h by averaging over K random shock series
-        h_values = []
         for _ in range(K):
-            #Generating a random shock series 
-            epsilon_series = np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon, size=120)
-            #Calculate the ex-post value of the salon for the generated shock series
-            h_value = self.calculate_h(epsilon_series)
-            #Store the calculated h value in a list
-            h_values.append(h_value)
-            #Calculate the average of the h values
-        return np.mean(h_values)
+            kappa, ell = self.simulate_shock_series()
+            salon_value = self.calculate_salon_value(kappa, ell)
+            total_value += salon_value
+
+        H = total_value / K
+        return H
     
 #question 3
 
 class HairSalonQ3:
-    def __init__(self, rho, eta, wage, iota, sigma_epsilon, R, delta):
+    def __init__(self, rho, iota, sigma_epsilon, R, eta, w, delta):
         self.rho = rho
-        self.eta = eta
-        self.wage = wage
         self.iota = iota
         self.sigma_epsilon = sigma_epsilon
         self.R = R
+        self.eta = eta
+        self.w = w
         self.delta = delta
 
-    def calculate_h(self, epsilon_series):
-        kappa_series = [1.0]  # Initial kappa
-        ell_series = [0]  # Initial ell
-        h_value = 0.0
+    def policy(self, kappa_t):
+        return ((1 - self.eta) * kappa_t / self.w) ** (1 / self.eta)
 
-        #Loop over the time periods
+    def simulate_shock_series(self):
+        epsilon = np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon, size=120)
+        kappa = [1]
+        ell = [0]
+
         for t in range(120):
-            # Calculate kappa_t based on AR(1) process
-            kappa_t = np.exp(self.rho * np.log(kappa_series[-1]) + epsilon_series[t])
-            kappa_series.append(kappa_t)
+            kappa_t = self.rho * np.log(kappa[t]) + epsilon[t]
+            ell_t = self.policy(kappa_t)
+            if t > 0 and abs(ell[t-1] - ell_t) > self.delta:
+                ell_t = ell_t
+            else:
+                ell_t = ell[t]
+            kappa.append(np.exp(kappa_t))
+            ell.append(ell_t)
 
-            # Calculate ell_t based on the policy with threshold delta
-            ell_star = ((1 - self.eta) * kappa_t / self.wage) ** (1 / self.eta)
-            if t > 0 and abs(ell_series[-1] - ell_star) > self.delta: #checks if t is greater then 0, 
-                #if the absolute difference between the previous value of ell and ell_star is greater than the value of delta.  
-                ell_t = ell_star #If the statement is true, a significant change in ell has occurred, 
-                #and the new value ell_t is set to ell_star.
-            else: #if the statement is false he change in ell is not significant, and the new value ell_t is set to the same 
-                #value in the previous time period, ell_series[-1].
-                ell_t = ell_series[-1]
-            ell_series.append(ell_t) #The updated value of ell_t is appended to the ell_series list. This shows the sequence 
-            #of ell values over time.
+        return kappa, ell
 
-            # Calculate the period profit
-            profit = kappa_t * ell_t ** (1 - self.eta) - self.wage * ell_t
-            adjustment_cost = self.iota if ell_t != ell_series[-2] else 0
-            period_value = profit - adjustment_cost
-            discounted_value = period_value * self.R ** (-t)
-            h_value += discounted_value
+    def calculate_salon_value(self, kappa, ell):
+        salon_value = sum([(self.R ** -t) * (kappa[t] * ell[t] ** (1 - self.eta) - self.w * ell[t] -
+                                              (ell[t] != ell[t-1]) * self.iota)
+                           for t in range(120)])
+        return salon_value
 
-        return h_value
+    def calculate_ex_ante_value(self, K):
+        total_value = 0
 
-    def calculate_expected_h(self, K):
-        h_values = [] #Create empty list, to store the values from the for loop
-        for _ in range(K): #iterates K loops, where _ is a placeholder 
-            epsilon_series = np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon, size=120) #a randome series 
-            #of epsilon values are generated 120 times, and drawend from a normal distrubution with a mean of -0.5*sigma_espilon**2 
-            #and standard diviation of sigma_epsilon
-            h_value = self.calculate_h(epsilon_series) #calculates h with the generated epsilon series
-            h_values.append(h_value) #append the calulated h values to the empty list
-        return np.mean(h_values) #returns the generated avarage 
+        for _ in range(K):
+            kappa, ell = self.simulate_shock_series()
+            salon_value = self.calculate_salon_value(kappa, ell)
+            total_value += salon_value
+
+        H = total_value / K
+        return H
     
 
 #Question 4
 
+
 class HairSalonoptimalQ4:
-    def __init__(self, rho, eta, wage, iota, sigma_epsilon, R):
+    def __init__(self, rho, iota, sigma_epsilon, R, eta, w, delta):
         self.rho = rho
-        self.eta = eta
-        self.wage = wage
         self.iota = iota
         self.sigma_epsilon = sigma_epsilon
         self.R = R
+        self.eta = eta
+        self.w = w
+        self.delta = delta
 
-    #Calculating the ex-post value of the hair salon for a given shock series and delta
-    def calculate_h(self, epsilon_series, delta):
-        kappa_series = [1.0]  # Initial kappa
-        ell_series = [0]  # Initial ell
-        h_value = 0.0
+    def policy(self, kappa_t):
+        return ((1 - self.eta) * kappa_t / self.w) ** (1 / self.eta)
+
+    def simulate_shock_series(self):
+        epsilon = np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon, size=120)
+        kappa = [1]
+        ell = [0]
 
         for t in range(120):
-            # Calculate kappa_t based on AR(1) process
-            kappa_t = np.exp(self.rho * np.log(kappa_series[-1]) + epsilon_series[t])
-            kappa_series.append(kappa_t)
-
-            # Calculate ell_t based on the policy with threshold delta
-            ell_star = ((1 - self.eta) * kappa_t / self.wage) ** (1 / self.eta)
-            if t > 0 and abs(ell_series[-1] - ell_star) > delta:
-                ell_t = ell_star
+            kappa_t = self.rho * np.log(kappa[t]) + epsilon[t]
+            ell_t = self.policy(kappa_t)
+            if t > 0 and abs(ell[t-1] - ell_t) > self.delta:
+                ell_t = ell_t
             else:
-                ell_t = ell_series[-1]
-            ell_series.append(ell_t)
+                ell_t = ell[t]
+            kappa.append(np.exp(kappa_t))
+            ell.append(ell_t)
 
-            # Calculate the period profit
-            profit = kappa_t * ell_t ** (1 - self.eta) - self.wage * ell_t
-            adjustment_cost = self.iota if ell_t != ell_series[-2] else 0
-            period_value = profit - adjustment_cost
-            discounted_value = period_value * self.R ** (-t)
-            h_value += discounted_value
+        return kappa, ell
 
-        return h_value
+    def calculate_salon_value(self, kappa, ell):
+        salon_value = sum([(self.R ** -t) * (kappa[t] * ell[t] ** (1 - self.eta) - self.w * ell[t] -
+                                              (ell[t] != ell[t-1]) * self.iota)
+                           for t in range(120)])
+        return salon_value
 
-    def objective_function(self, delta):
-
-        #We minimize the objective function in order to find the optimal delta 
-        K = 1000  # Number of shock series to simulate
-        h_values = []
+    def calculate_ex_ante_value(self, K):
+        total_value = 0
 
         for _ in range(K):
-            epsilon_series = np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon, size=120)
-            h_value = self.calculate_h(epsilon_series, delta)
-            h_values.append(h_value)
+            kappa, ell = self.simulate_shock_series()
+            salon_value = self.calculate_salon_value(kappa, ell)
+            total_value += salon_value
 
-        return -np.mean(h_values)  # Negative to maximize the objective function
-    
-    def plot_h_values(self, delta_values, h_values):
-        plt.plot(delta_values, h_values)
-        plt.xlabel('Delta')
-        plt.ylabel('H')
-        plt.title('Optimal Delta vs H')
-        plt.show()
-
-    def find_optimal_delta(self, delta_lower_bound, delta_upper_bound):
-        result = minimize_scalar(self.objective_function, bounds=(delta_lower_bound, delta_upper_bound), method='bounded')
-        optimal_delta = result.x
-        optimal_h = -result.fun
-        return optimal_delta, optimal_h
+        H = total_value / K
+        return H
     
 
 #Question 5
 
+
 class HairSalonDynamicQ5:
-    def __init__(self, base_price):
-        self.base_price = base_price
-
-    #Calculating the dynamically adjusted price based on demand level
-    def calculate_dynamic_price(self, demand_level):
-        # Implement the dynamic pricing algorithm here
-        # Use the demand level to adjust the base price
-        # Return the dynamically adjusted price
-
-        dynamic_price = self.base_price * (1 + demand_level)  # Adjust the price based on demand level
-
-        return dynamic_price
-
-class DemandDataQ5:
-    def __init__(self, num_periods, rho, sigma_epsilon):
-        self.num_periods = num_periods
+    def __init__(self, rho, iota, sigma_epsilon, R, eta, w, delta):
         self.rho = rho
+        self.iota = iota
         self.sigma_epsilon = sigma_epsilon
+        self.R = R
+        self.eta = eta
+        self.w = w
+        self.delta = delta
 
-    def generate_epsilon_series(self):
-        #Generate a series of epsilon values based on an AR(1) process
-        epsilon_series = []
-        epsilon_t = np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon)
+    def policy(self, kappa_t):
+        return ((1 - self.eta) * kappa_t / self.w) ** (1 / self.eta)
 
-        for _ in range(self.num_periods):
-            epsilon_series.append(epsilon_t)
-            epsilon_t = self.rho * epsilon_t + np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon)
+    def dynamic_pricing(self, kappa_t, ell_t, desired_profit_margin):
+        if ell_t == 0:
+            price = self.w * (1 + desired_profit_margin)
+        else:
+            price = kappa_t * ell_t ** (-self.eta) * (1 + desired_profit_margin)
+        return price
 
-        return epsilon_series
+    def simulate_shock_series(self):
+        epsilon = np.random.normal(-0.5 * self.sigma_epsilon ** 2, self.sigma_epsilon, size=120)
+        kappa = [1]
+        ell = [0]
+
+        for t in range(120):
+            kappa_t = self.rho * np.log(kappa[t]) + epsilon[t]
+            ell_t = self.policy(kappa_t)
+            if t > 0 and abs(ell[t-1] - ell_t) > self.delta:
+                ell_t = ell_t
+            else:
+                ell_t = ell[t]
+            kappa.append(np.exp(kappa_t))
+            ell.append(ell_t)
+
+        return kappa, ell
+
+    def calculate_salon_value(self, kappa, ell, desired_profit_margin):
+        salon_value = sum([(self.R ** -t) * (self.dynamic_pricing(kappa[t], ell[t], desired_profit_margin) * ell[t] -
+                                              self.w * ell[t] - (ell[t] != ell[t-1]) * self.iota)
+                           for t in range(120)])
+        return salon_value
+
+    def calculate_ex_ante_value(self, K, desired_profit_margin):
+        total_value = 0
+
+        for _ in range(K):
+            kappa, ell = self.simulate_shock_series()
+            salon_value = self.calculate_salon_value(kappa, ell, desired_profit_margin)
+            total_value += salon_value
+
+        H = total_value / K
+        return H
 
 
